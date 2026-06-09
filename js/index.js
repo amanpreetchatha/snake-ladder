124cce1e6735336a0e93bb1918242b7837de761e
amanpreetchatha <amanpreetchatha99@gmail.com>
2026-06-09 00:36:21 -0400

    more changes to ui

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 
-import formDiv from "./playerForm.js";
+import {formDiv, thirdPlayer, fourthPlayer} from "./playerForm.js";
 
 let playButtonDiv = document.querySelector(".button-div");
 let mainElement = document.querySelector("main");
@@ -17,5 +17,21 @@ document.querySelector("#play-button").addEventListener("click",(e)=>{
     document.querySelector("#exit-game").addEventListener("click", (e) => {
         mainElement.replaceChildren(withPlayer,withComp,playButtonDiv);
     });
-
+    let fieldset = document.querySelector(".radio");
+    fieldset.addEventListener("change",(event)=>{
+        if(event.target && event.target.name === "player-count"){
+            if(event.target.value ==="two"){
+                document.querySelector(".ic3").innerHTML = "";
+                document.querySelector(".ic4").innerHTML = "";
+            }
+            if (event.target.value === "three") {
+                document.querySelector(".ic4").innerHTML = "";
+                document.querySelector(".ic3").innerHTML = thirdPlayer;
+            }
+            if (event.target.value === "four") {
+                document.querySelector(".ic3").innerHTML = thirdPlayer;
+                document.querySelector(".ic4").innerHTML = fourthPlayer;
+            }
+        }
+    })
 });
--- js/playerForm.js
+++ js/playerForm.js
@@ -3,36 +3,46 @@ let formDiv = `
 <div class="form">
     <div class="title">Choose Color and Name</div>
     <div class="input-container ic1">
-        <input id="firstname" class="input" type="text" placeholder=" " />
+        <input id="player1" class="input" type="text" placeholder=" " /><img class="player-icon" src="./img/icon-red.webp"/>
         <div class="cut"></div>
-        <label for="firstname" class="placeholder">Player 1</label>
+        <label for="player1" class="placeholder">Player 1</label>
+        
     </div>
     <div class="input-container ic2">
-        <input id="lastname" class="input" type="text" placeholder=" " />
+        <input id="player2" class="input" type="text" placeholder=" " /><img class="player-icon" src="./img/icon-red.webp"/>
         <div class="cut"></div>
-        <label for="lastname" class="placeholder">Player 2</label>
+        <label for="player2" class="placeholder">Player 2</label>
     </div>
-    <fieldset>
+    <div class="ic3"></div>
+    <div class="ic4"></div>
+    <fieldset class="radio">
         <legend>Number of Players:</legend>
-
-        <div>
             <input type="radio" id="two" name="player-count" value="two" checked />
             <label for="two">Two</label>
-        </div>
-
-        <div>
             <input type="radio" id="three" name="player-count" value="three" />
             <label for="three">Three</label>
-        </div>
-
-        <div>
             <input type="radio" id="four" name="player-count" value="four" />
             <label for="four">Four</label>
-        </div>
     </fieldset>
 
     <button type="text" class="btn play" id="start-game">Start</button>
     <button type="text" class="btn exit" id="exit-game">Exit</button>
 </div>
 `;
-export default formDiv;
+
+let thirdPlayer = `
+    <div class="input-container">
+        <input id="player3" class="input" type="text" placeholder=" " />
+        <div class="cut"></div>
+        <label for="player3" class="placeholder">Player 3</label>
+    </div>
+`;
+
+let fourthPlayer = `
+    <div class="input-container">
+        <input id="player4" class="input" type="text" placeholder=" " />
+        <div class="cut"></div>
+        <label for="player4" class="placeholder">Player 4</label>
+    </div>
+`;
+export {formDiv, thirdPlayer, fourthPlayer};
